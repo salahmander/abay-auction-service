2f8699798e54fb9f8a12376d34cf03de7a5ca3cc
salahmander <salahabdo@hotmail.co.uk>
2022-02-08 09:42:18 +0000

    Validate the auction and prevent lower bids than the current bid from going through.

--- abay/src/handlers/placeBid.js
+++ abay/src/handlers/placeBid.js
@@ -1,12 +1,23 @@
 import AWS from "aws-sdk";
 import commonMiddleware from "../middleware/commonMiddleware";
 import createError from "http-errors";
+import { getAuctionById } from "./getAuction";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function placeBid(event, context) {
   const { id } = event.pathParameters;
   const { amount } = event.body;
+
+  // Validate if the auction exists
+  const auction = await getAuctionById(id);
+
+  // Prevent bids that are lower than the current bid
+  if (amount <= auction.highestBid.amount) {
+    throw new createError.Forbidden(
+      `Your bid must be higher than ${auction.highestBid.amount}!`
+    );
+  }
 
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
